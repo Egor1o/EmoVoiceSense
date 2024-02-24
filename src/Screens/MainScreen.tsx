354f7c92247bf074ac4e947ad9839e6c7e9dcab0
Egor1o <egorklo147@gmail.com>
2024-02-24 12:56:25 +0200

    This is baza

---
+++ src/Screens/MainScreen.tsx
@@ -0,0 +1,12 @@
+import RecordingButton from "../Components/MainScreenComponents/RecordingButton.tsx";
+
+
+const MainScreen = () => {
+
+    const button = () => {}
+    return (<div>
+        <RecordingButton onPress={button}/>
+    </div>)
+}
+
+export default MainScreen
