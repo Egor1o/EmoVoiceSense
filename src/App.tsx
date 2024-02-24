4693f20307db63a4604f7ece764dc96d4d061190
Dereden399 <denisk399@gmail.com>
2024-02-24 13:05:55 +0200

    Configured react-py

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@
 import './App.css'
 import MainScreen from "./Screens/MainScreen.tsx";
 import { PythonProvider } from 'react-py'
+import {useEffect} from "react";
 
 function App() {
-
+  useEffect(() => {
+    navigator.serviceWorker
+      .register('/react-py-sw.js')
+      .then((registration) =>
+        console.log(
+          'Service Worker registration successful with scope: ',
+          registration.scope
+        )
+      )
+      .catch((err) => console.log('Service Worker registration failed: ', err))
+  }, [])
   return (
     <>
       <PythonProvider>
